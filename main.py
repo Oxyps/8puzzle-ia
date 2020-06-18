5cf8a31497df15043737923b5e1961e4a08b4e4a
Oxyps <abel_castro_junior@hotmail.com>
2020-06-18 20:12:15 -0300

    refined show path function and numpy import syntax

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from random import choice
 from state import State
 
+def path():
+	path = []
+
+	current_state = FINAL_STATE
+	while current_state.parent:
+		path.insert(0, current_state)
+		current_state = current_state.parent
+	path.insert(0, INITIAL_STATE)
+	
+	return path
+
 def show(state):
 	for line in state.matrix:
 		for value in line:
@@ -15,11 +26,14 @@ def show_path():
 	print('ESTADO INICIAL:')
 	show(INITIAL_STATE)
 	
-	print('\nESTADO OBJETIVO:')
+	print('ESTADO OBJETIVO:')
 	show(GOAL_STATE)
 
-	print(f'\nNÚMERO DE PASSOS: {len(closed_states) - 1}\nCAMINHO ENCONTRADO:')
-	for state in closed_states:
+	print(f'NÚMERO DE ESTADOS FECHADOS: {len(closed_states)}')
+	print(f'NÚMERO DE ESTADOS EM ABERTO: {len(opened_states)}')
+
+	print(f'NÚMERO DE PASSOS ATÉ O OBJETIVO: {len(path())}\n\nCAMINHO ENCONTRADO:')
+	for state in path():
 		show(state)
 
 def h1(current_matrix, goal_matrix):
@@ -36,7 +50,7 @@ def h1(current_matrix, goal_matrix):
 
 def h2(current_matrix, goal_matrix):
 	""" This heuristic return the Manhattan distance for each tile out'o position """
-	
+
 	current_array = []
 	for row in current_matrix:
 		for item in row:
@@ -56,84 +70,73 @@ def f(current_state, goal_state):
 		Where g(x) means the current state level in graph\n
 		and the h(x) an heuristic cost to the current state to the goal state"""
 
-	# return current_state.level + h1(current_state.matrix, goal_state.matrix)
-	return current_state.level + h2(current_state.matrix, goal_state.matrix)
-
-def random_state():
-	choose = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-
-	matrix = []
-
-	for i in range(3):
-		line = []
-		for j in range(3):
-			chosen = choice(choose)
-			line.append(chosen)
-			choose.remove(chosen)
-		matrix.append(line)
-
-	return State(matrix, 0, 0)
+	return current_state.level + h1(current_state.matrix, goal_state.matrix)
+	# return current_state.level + h2(current_state.matrix, goal_state.matrix)
 
 def astar():
 	while True:
-		# remove from the start cause its upward ordered, so the start is the min fvalue
+		# remove from start of opened states cause its upward ordered, so the start is the min fvalue
 		current_state = opened_states[0]
-
-		# close current state and delete from opened states list
-		closed_states.append(current_state)
 		del opened_states[0]
+
+		# close current state
+		closed_states.append(current_state)
 
 		# if the heuristic cost between the current matrix to the goal matrix is 0
 		# then the best path, for the a* search with this heuristic, was achieved
-		# if h1(current_state.matrix, GOAL_STATE.matrix) == 0:
-		# 	break
-		if h2(current_state.matrix, GOAL_STATE.matrix) == 0:
-			break
+		if h1(current_state.matrix, GOAL_STATE.matrix) == 0:
+			return current_state
+		# if h2(current_state.matrix, GOAL_STATE.matrix) == 0:
+		# 	return current_state
 
 		# calculate fvalue to all child states from current state
 		for child in current_state.generate_children():
 			child.fvalue = f(child, GOAL_STATE)
+			child.parent = current_state
 			opened_states.append(child)
 
 		# upward sort opened states by fvalue
 		opened_states.sort(key=lambda state: state.fvalue)
 
-def dfs():
-	while not opened_states.__eq__([]):
-		# remove from the end
-		current_state = opened_states[-1]
-
-		# close current state and delete from opened states list
-		closed_states.append(current_state)
-		del opened_states[-1]
-
-		# if the current state equals final state matrix
-		# then the best path for the dfs search was achieved
-		if current_state.__eq__(GOAL_STATE):
-			break
-
-		# open the child states from current state
-		for child in current_state.generate_children():
-			if not (child.already_exists(opened_states) and child.already_exists(closed_states)):
-				opened_states.append(child)
-
 def bfs():
 	while not opened_states.__eq__([]):
-		# remove from the beginning
+		# remove from begin of opened states
 		current_state = opened_states[0]
-
-		# close current state and delete from opened states list
-		closed_states.append(current_state)
 		del opened_states[0]
+
+		# close current state
+		closed_states.append(current_state)
 
 		# if the current state equals final state matrix
 		# then the best path for the bfs search was achieved
 		if current_state.__eq__(GOAL_STATE):
-			break
+			return current_state
+
+		# open the child states from current state
+		for child in current_state.generate_children():
+			# if not child.already_exists(opened_states) or not child.already_exists(closed_states):
+			if not (child.already_exists(opened_states) and child.already_exists(closed_states)):
+				child.parent = current_state
+				opened_states.append(child)
+
+def dfs():
+	while not opened_states.__eq__([]):
+		# remove from end of opened states
+		current_state = opened_states[-1]
+		del opened_states[-1]
+
+		# close current state
+		closed_states.append(current_state)
+
+		# if the current state equals final state matrix
+		# then the best path for the dfs search was achieved
+		if current_state.__eq__(GOAL_STATE):
+			return current_state
 
 		# open the child states from current state
 		for child in current_state.generate_children():
 			if not (child.already_exists(opened_states) and child.already_exists(closed_states)):
+				child.parent = current_state
 				opened_states.append(child)
 
 def issolvable():
@@ -154,7 +157,22 @@ def issolvable():
 	# if the number of inversions is even then is solvable
 	return inversion_count % 2 == 0
 
-INITIAL_STATE = State([[2, 8, 3], [1, 6, 4], [7, 0, 5]], 0, 0)
+def random_state():
+	choose = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+
+	matrix = []
+
+	for i in range(3):
+		line = []
+		for j in range(3):
+			chosen = choice(choose)
+			line.append(chosen)
+			choose.remove(chosen)
+		matrix.append(line)
+
+	return State(matrix, 0, 0, None)
+
+INITIAL_STATE = State([[2, 8, 3], [1, 6, 4], [7, 0, 5]], 0, 0, None)
 """ estados iniciais aleatórios
 INITIAL_STATE = None
 while True:
@@ -163,17 +181,17 @@ while True:
 		break
 """
 
-GOAL_STATE = State([[1, 2, 3], [8, 0, 4], [7, 6, 5]], 0, 0)
+GOAL_STATE = State([[1, 2, 3], [8, 0, 4], [7, 6, 5]], 0, 0, None)
 
 INITIAL_STATE.fvalue = f(INITIAL_STATE, GOAL_STATE)
 
 opened_states = [INITIAL_STATE]
 closed_states = []
 
-astar()
 """ rodar uma busca por vez
-dfs()
-bfs()
+FINAL_STATE = bfs()
+FINAL_STATE = dfs()
 """
+FINAL_STATE = astar()
 
 show_path()
--- state.py
+++ state.py
@@ -1,31 +1,41 @@
-import numpy as np
+from numpy import array, array_equal, where
 
 class State:
-	def __init__(self, matrix, level, fvalue):
-		self.matrix = np.array(matrix)
+	def __init__(self, matrix, level, fvalue, parent):
+		self.matrix = array(matrix)
 		self.level = level
 		self.fvalue = fvalue
+		self.parent = parent
 
 	def __str__(self):
 		return str(self.matrix)
 
 	def __eq__(self, state):
-		return np.array_equal(self.matrix, state.matrix)
+		if state.fvalue != self.fvalue:
+			return False
+
+		return array_equal(self.matrix, state.matrix)
+	
+	def __ne__(self, state):
+		return not array_equal(self.matrix, state.matrix)
+
+	def __hash__(self):
+		return hash(tuple(row) for row in self.matrix) * hash(self.fvalue) * hash(self.level)
 
 	def already_exists(self, states_list):
 		""" Check if the all state is in given list of states """
-		
+
 		return self in states_list
 
 	def find_blank(self):
 		""" Returns the position of the blank space """
 
-		return np.where(self.matrix == 0)
-	
+		return where(self.matrix == 0)
+
 	def move(self, x1, y1, x2, y2):
 		""" Move the blank (x1, y1) to the given direction (x2, y2)\n
 		if the given direction its out of limits return None """
-		
+
 		if x2 >= 0 and x2 < 3 and y2 >= 0 and y2 < 3:
 			temp_matrix = self.matrix.copy()
 
@@ -33,10 +43,10 @@ class State:
 
 			temp_matrix[x2, y2] = 0
 			temp_matrix[x1, y1] = temp
-			
+
 			return temp_matrix
-		else:
-			return None
+
+		return None
 
 	def generate_children(self):
 		""" To generate child states from the given one by moving
@@ -51,7 +61,7 @@ class State:
 			child_matrix = self.move(x, y, direction[0], direction[1])
 
 			if child_matrix is not None:
-				child_state = State(child_matrix, self.level + 1, 0)
+				child_state = State(child_matrix, self.level + 1, 0, None)
 				children.append(child_state)
 
 		return children
